c9e22a07d46b4d22be6a3fc40ad968b8792dd10c
Regina <rmyachina@mail.ru>
2023-11-13 00:16:35 +0300

    listing

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import Listing from './components/Listing/Listing';
 import etsy from './data/etsy.json';
 
+export type ETSY = typeof etsy;
+export type ETSYItem = typeof etsy[0];
 
 function App() {
   return (
-    <Listing items={etsy}/>
+    <Listing items={etsy} />
   );
 }
 
 export default App;
+
 
--- src/components/Listing/Listing.tsx
+++ src/components/Listing/Listing.tsx
@@ -1,48 +1,107 @@
-import React, { useState } from "react";
-
-
-export interface TItem {
-    listing_id: number,
-    url: string,
-    MainImage: {
-      url_570xN: string,
-    },
-    title: string,
-    currency_code: string,
-    price: string,
-    quantity: number,
-}
-
+import React from "react";
+import { ETSY } from "../../App";
 
 type ListingProps = {
-  items: TItem[],
+  items: ETSY
 }
 
 type ItemProps = {
-    item: TItem,
+  url?: string,
+  MainImage?: {
+    url_570xN: string
+  }
+  title?: string
+  currency_code?: string
+  price?: string
+  quantity?: number
 }
 
+type TitleProps = {
+  title: string,
+}
 
-export default function Listing({items}:ListingProps) {
+type PriceProps = {
+  price: string,
+  currency_code: string
+}
 
-    return <div className="item-list"></div>
-} 
+type QuantityProps = {
+  quantity: number
+}
+
+export default function Listing({ items }: ListingProps) {
+
+  return (
+    <div className="item-list">
+      {
+        items.map((el) => (
+          el.state !== 'removed' ?
+            <Item
+              key={el.listing_id}
+              url={el.url}
+              MainImage={el.MainImage}
+              title={el.title}
+              currency_code={el.currency_code}
+              price={el.price}
+              quantity={el.quantity} /> : ""
+        ))
+      }
+    </div>
+  )
+}
+
+function Item({
+  url,
+  MainImage,
+  title,
+  currency_code,
+  price,
+  quantity, }: ItemProps) {
+
+  return (
+    <div className="item">
+      <div className="item-image">
+        <a href={url}>
+          <img src={MainImage?.url_570xN} alt="/" />
+        </a>
+      </div>
+      <div className="item-details">
+        <p className="item-title">{
+          title ? <Title title={title} /> : ""
+        }</p>
+        <p className="item-price">{
+          price && currency_code ? <Price price={price} currency_code={currency_code} /> : ""
+        }</p>{
+          quantity ? <Quantity quantity={quantity} /> : ""
+        }
+      </div>
+    </div>
+  )
+}
+
+function Quantity({ quantity }: QuantityProps) {
+  return (
+    quantity <= 10 ? <p className="item-quantity level-low">{quantity} left</p> :
+      quantity <= 20 ? <p className="item-quantity level-medium">{quantity} left</p> :
+        <p className="item-quantity level-high">{quantity} left</p>
+  )
+}
+
+function Price({ price, currency_code }: PriceProps) {
+  return (
+    currency_code === "USD" ? <>{`$${price}`}</> :
+      currency_code === "EUR" ? <>{`€${price}`}</> :
+        <>{price}{currency_code}</>
+  )
+}
+
+function Title({ title }: TitleProps) {
+  return (
+    title.length > 50 ? <>{`${title.slice(0, 50)}...`}</> : <>{title}</>
+  )
+}
 
 Listing.defaultProps = {
   items: [],
 }
 
-export function Item({item}:ItemProps) {
-    <div className="item">
-    <div className="item-image">
-      <a href={item.url}>
-        <img src={item.MainImage.url_570xN} alt="/"/>
-      </a>
-    </div>
-    <div className="item-details">
-      <p className="item-title">{item.title.length <= 50 ? item.title : `${item.title.slice(0,50)}...`}</p>
-      <p className="item-price">{item.price}</p>
-      <p className="item-quantity level-medium">{item.quantity}</p>
-    </div>
-  </div>
-}
